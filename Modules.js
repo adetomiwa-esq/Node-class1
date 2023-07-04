3039f6ff200fc65e6d6a07541f27b8e7c9b0c7a3
Akinola Kehinde <khennyakin@gmail.com>
2023-07-04 11:53:38 +0100

    first class files

---
+++ Modules.js
@@ -0,0 +1,3 @@
+const {ages, people} = require('./People')
+
+console.log(ages)
---
+++ People.js
@@ -0,0 +1,6 @@
+const people = ['Ade', 'Ayo', 'Mike']
+const ages = [8, 6, 9]
+
+module.exports = {people, ages}
+
+//console.log(people)
