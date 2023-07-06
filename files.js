e9cef4948e45248230438b9a11670d9ad435f509
Akinola Kehinde <khennyakin@gmail.com>
2023-07-06 11:23:52 +0100

    node fs practice

--- files.js
+++ files.js
@@ -34,9 +34,51 @@ if(fs.existsSync('./docs/deleteme.txt')){
     })
 }
 
-fs.writeFile('./docs/script.js', 'console.log("hello world")', (err) => {
+fs.writeFile('./docs/script.js', 'console.log("hello server")', (err) => {
     if(err){
         console.log(err)
     }
     console.log('js file created')
 })
+
+fs.readFile('./docs/script.js', (err,data) => {
+    if(err){
+        console.log(err)
+    }
+    console.log(data.toString())
+})
+
+fs.readFile('./docs/blog2.txt', (err, data) => {
+    if(err){
+        console.log(err)
+    }
+    console.log(data.toString())
+})
+
+fs.writeFile('./docs/blog3.txt', "let's rearrange what we have or create a new one", (err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log('creating and writing in blog3')
+})
+
+fs.unlink('./docs/blog3.txt', () => {
+    console.log('delete blog3')
+})
+
+if(!fs.existsSync('./docs/folder')){
+    fs.mkdir('./docs/folder', (err) => {
+        if(err){
+            console.log(err)
+        }
+        console.log('folder created')
+    })
+}
+else{
+    fs.rmdir('./docs/folder', (err) => {
+        if(err){
+            console.log(err)
+        }
+        console.log('folder removed')
+    })
+}
